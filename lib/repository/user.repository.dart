6ddd9ab89c18faa4cbdbd97b3c3062d95adb22ce
luccasrojas <l.rojasb@uniandes.edu.co>
2023-09-20 16:50:53 -0500

    user repo

--- lib/repository/user.repository.dart
+++ lib/repository/user.repository.dart
@@ -3,14 +3,19 @@ import 'package:hive_app/data/remote/network/NetworkApiService.dart';
 import 'package:hive_app/data/remote/network/ApiEndPoints.dart';
 import 'package:hive_app/data/remote/network/BaseApiService.dart';
 
-class UserRepo {
+
+abstract class UserRepo {
+  Future<UserModel?> getUserData();
+}
+
+class UserRepoImpl extends UserRepo {
 
   final BaseApiService _apiService = NetworkApiService();
 
   @override
   Future<UserModel?> getUserData() async {
     try {
-      dynamic response = await _apiService.getResponse(ApiEndPoints().USER_DATA);
+      dynamic response = await _apiService.getResponse(ApiEndPoints().usersEndPoint);
       print("Log: $response");
       final jsonData = UserModel.fromJson(response);
       return jsonData;
