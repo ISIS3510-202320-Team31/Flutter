c98e8fee7bcc4322d484b5d8129e00cf689da599
luccasrojas <l.rojasb@uniandes.edu.co>
2023-09-22 23:37:49 -0500

    list of events overflow fix

--- lib/view/pages/Home.dart
+++ lib/view/pages/Home.dart
@@ -6,10 +6,10 @@ import 'package:hive_app/view/pages/Create.dart';
 import 'package:hive_app/view/pages/Feed.dart';
 
 class Home extends StatefulWidget {
-  const Home({super.key});
+  const Home({Key? key});
 
   @override
-State<Home> createState() =>_NavState();
+  State<Home> createState() => _NavState();
 }
 
 class _NavState extends State<Home> {
@@ -32,65 +32,69 @@ class _NavState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: _widgetOptions.elementAt(_selectedIndex)
+        child: _widgetOptions.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
-          items: <BottomNavigationBarItem>[
+        items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-              activeIcon: Icon(
-                Icons.home,
-                color: appTheme.focusColor,
-              ),
-              icon: Icon(
-                Icons.home,
-                color: appTheme.unselectedWidgetColor,
-              ),
-              label: "Inicio"),
+            activeIcon: Icon(
+              Icons.home,
+              color: appTheme.focusColor,
+            ),
+            icon: Icon(
+              Icons.home,
+              color: appTheme.unselectedWidgetColor,
+            ),
+            label: "Inicio",
+          ),
           BottomNavigationBarItem(
-              activeIcon: Icon(
-                Icons.add_circle,
-                color: appTheme.focusColor,
-              ),
-              icon: Icon(
-                Icons.add_circle,
-                color: appTheme.unselectedWidgetColor,
-              ),
-              label: "Crear"),
+            activeIcon: Icon(
+              Icons.add_circle,
+              color: appTheme.focusColor,
+            ),
+            icon: Icon(
+              Icons.add_circle,
+              color: appTheme.unselectedWidgetColor,
+            ),
+            label: "Crear",
+          ),
           BottomNavigationBarItem(
-              activeIcon: Icon(
-                Icons.calendar_month,
-                color: appTheme.focusColor,
-              ),
-              icon: Icon(
-                Icons.calendar_month,
-                color: appTheme.unselectedWidgetColor,
-              ),
-              label: "Calendario"),
+            activeIcon: Icon(
+              Icons.calendar_month,
+              color: appTheme.focusColor,
+            ),
+            icon: Icon(
+              Icons.calendar_month,
+              color: appTheme.unselectedWidgetColor,
+            ),
+            label: "Calendario",
+          ),
           BottomNavigationBarItem(
-              activeIcon: Icon(
-                Icons.account_circle,
-                color: appTheme.focusColor,
-              ),
-              icon: Icon(
-                Icons.account_circle,
-                color: appTheme.unselectedWidgetColor,
-              ),
-              label: "Perfil")],
-          unselectedItemColor: Colors.black,
-          selectedItemColor: appTheme.focusColor,
-          type: BottomNavigationBarType.shifting,
-          showUnselectedLabels: false,
-          showSelectedLabels: true,
-          currentIndex: _selectedIndex,
-          elevation: 1.5,
-          onTap: (int index) {
-            if (index != _selectedIndex)
-              setState(() {
-                _selectedIndex = index;
-              });
-          },
-        )
+            activeIcon: Icon(
+              Icons.account_circle,
+              color: appTheme.focusColor,
+            ),
+            icon: Icon(
+              Icons.account_circle,
+              color: appTheme.unselectedWidgetColor,
+            ),
+            label: "Perfil",
+          ),
+        ],
+        unselectedItemColor: Colors.black,
+        selectedItemColor: appTheme.focusColor,
+        type: BottomNavigationBarType.shifting,
+        showUnselectedLabels: false,
+        showSelectedLabels: true,
+        currentIndex: _selectedIndex,
+        elevation: 1.5,
+        onTap: (int index) {
+          if (index != _selectedIndex)
+            setState(() {
+              _selectedIndex = index;
+            });
+        },
+      ),
     );
   }
 }
-
