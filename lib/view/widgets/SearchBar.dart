b5888727a0b125af748bb3a271e1acf62b89b595
Nicolás Rueda <ne.rueda@uniandes.edu.co>
2023-09-25 19:05:52 -0500

    MiniUpdate: 2 buttons
    Button for camera and filter added

--- lib/view/widgets/SearchBar.dart
+++ lib/view/widgets/SearchBar.dart
@@ -3,23 +3,37 @@ import 'package:hive_app/utils/ColorPalette.dart';
 
 
 class Search extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return
-    Container(
-      width: double.infinity,
+    return Container(
       padding: EdgeInsets.symmetric(horizontal: 30, vertical: 50),
       child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(height: 30),
-          // Centra la barra de búsqueda
-          Container(
-            width: 310, // LONGITUD DE LA BARRA DE BUSQUEDA
-            child: Material(
-              elevation: 5.0,
-              borderRadius: BorderRadius.all(Radius.circular(30)),
+          // Icono de filtro a la izquierda
+          IconButton(
+            icon: Icon(
+              Icons.filter_list,
+              color: appTheme.focusColor,
+            ),
+            onPressed: () {
+              // Acción que deseas ejecutar cuando se presiona el botón izquierdo
+            },
+          ),
+          // Barra de búsqueda que ocupa el espacio disponible
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.only(left: 10, right: 10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    offset: Offset(0, 3),
+                    blurRadius: 5,
+                  ),
+                ],
+              ),
               child: TextField(
                 style: TextStyle(
                   fontSize: 16.0,
@@ -28,15 +42,19 @@ class Search extends StatelessWidget {
                 cursorColor: appTheme.primaryColor,
                 decoration: InputDecoration(
                   border: InputBorder.none,
-                  contentPadding: EdgeInsets.symmetric(
-                    horizontal: 15, vertical: 13),
+                  hintText: "Buscar...",
+                  hintStyle: TextStyle(
+                    color: Colors.grey,
+                  ),
                   suffixIcon: Material(
                     child: InkWell(
                       child: Icon(
                         Icons.search,
                         color: Color.fromARGB(255, 33, 150, 243),
                       ),
-                      onTap: () {},
+                      onTap: () {
+                        // Acción que deseas ejecutar cuando se presiona el botón de búsqueda
+                      },
                     ),
                     elevation: 2.0,
                     borderRadius: BorderRadius.all(Radius.circular(30)),
@@ -44,6 +62,16 @@ class Search extends StatelessWidget {
                 ),
               ),
             ),
+          ),
+          // Icono a la derecha
+          IconButton(
+            icon: Icon(
+              Icons.center_focus_weak,
+              color: appTheme.focusColor,
+            ),
+            onPressed: () {
+              // Acción que deseas ejecutar cuando se presiona el botón derecho
+            },
           ),
         ],
       ),
