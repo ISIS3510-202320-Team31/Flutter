4edcc5c1e99520e5f15a370f7a118c1b9358f686
luccasrojas <l.rojasb@uniandes.edu.co>
2023-10-19 03:51:54 -0500

    cache for refreshing update fails when loading first time

--- lib/view/pages/Feed.dart
+++ lib/view/pages/Feed.dart
@@ -1,10 +1,15 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:hive_app/models/event.model.dart';
 import 'package:hive_app/utils/ColorPalette.dart';
 import 'package:hive_app/view/widgets/EventList.dart';
 import 'package:hive_app/view_model/event.vm.dart';
 import 'package:hive_app/view/widgets/SearchBar.dart';
 import 'package:hive_app/data/remote/response/Status.dart';
 import 'package:provider/provider.dart';
+import 'package:hive_app/utils/SecureStorage.dart';
+
 
 class Feed extends StatefulWidget {
   static const String id = "feed_screen";
@@ -18,7 +23,10 @@ class Feed extends StatefulWidget {
 
 class _FeedState extends State<Feed> {
   final EventVM eventVM = EventVM();
+  final SecureStorage secureStorage = SecureStorage();
+  var cachedEvents;
   late final void Function() updateFunction;
+
   @override
   void initState() {
     void Function() updateFunction(String userId) {
@@ -28,7 +36,21 @@ class _FeedState extends State<Feed> {
     }
     this.updateFunction = updateFunction(widget.userId);
     this.updateFunction();
+    this.getLocalEvents();
     super.initState();
+  }
+
+  void getLocalEvents() {
+    secureStorage.readSecureData("feedEvents").then((eventsJSON){
+      if (eventsJSON != null && eventsJSON.isNotEmpty) {
+        var eventsRaw = json.decode(eventsJSON);
+        var events = json.encode(eventsRaw['events']);
+        this.cachedEvents = eventModelFromJson(events).events;
+      }
+      else {
+        this.cachedEvents = [];
+      }
+    });
   }
 
   @override
@@ -51,12 +73,21 @@ class _FeedState extends State<Feed> {
                 switch (viewModel.eventModel.status) {
                   case Status.LOADING:
                     print("Log :: LOADING");
-                    return Container(
-                      child: Center(
-                        child: CircularProgressIndicator(),
-                      ),
-                      height: MediaQuery.of(context).size.height * 0.7,
-                    );
+                    this.getLocalEvents();
+                    return Expanded(child:
+                    Column(
+                      children: [
+                        Center(
+                        child: LinearProgressIndicator(),
+                        ),  
+                        Expanded(
+                        child: EventList(
+                            userId: widget.userId,
+                            eventList: this.cachedEvents,
+                            eventVM: eventVM,
+                            updateFunction: this.updateFunction))
+                      ]
+                    ));
                   case Status.ERROR:
                     print("Log :: ERROR");
                     return Container(
@@ -66,6 +97,7 @@ class _FeedState extends State<Feed> {
                     );
                   case Status.COMPLETED:
                     print("Log :: COMPLETED");
+                    secureStorage.writeSecureData('feedEvents', eventModelToJson(viewModel.eventModel.data!));
                     return Expanded(
                         child: EventList(
                             userId: widget.userId,
@@ -81,5 +113,6 @@ class _FeedState extends State<Feed> {
         ],
       ),
     );
+    
   }
 }
